e9ee1014798bf89dce0820bde8e306ed6c440407
Itsmewho <jordy_mt@live.nl>
2024-12-10 16:11:24 +0100

    models

--- models/all_models.py
+++ models/all_models.py
@@ -1,1 +1,25 @@
 # Models
+from pydantic import BaseModel, EmailStr, Field
+
+
+class RegisterModel(BaseModel):
+    name: str = Field(..., min_length=3)
+    surname: str = Field(..., min_length=3)
+    email: EmailStr
+    password: str = Field(..., min_length=4)
+    sec_password: str = Field(..., min_length=4)
+
+
+class UserModel(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+    password: str
+    role: str
+
+
+class AdminModel(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+    role: str
